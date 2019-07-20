9d2d5a6f12c11fc0454a552035a3caaf0360f432
huwentao <hu.wentao@outlook.com>
2019-07-20 17:55:55 +0800

    处于结构上的考虑, 将升级逻辑放回 SettingsPageBloc中 \n目前仍然存在一个问题: app退出后, 设备尚未断开连接, 但StreamBuilder中的initialData会导致app显示"尚未连接设备"

--- lib/bloc/settings_page_bloc.dart
+++ lib/bloc/settings_page_bloc.dart
@@ -11,18 +11,42 @@ class SettingsPageBloc extends BaseBloc {
   @override
   void dispose() {
     _transportDevice.close();
-//    _updateFirmware.close();
+    _updateFirmware.close();
   }
+
   // 设备连接 事件的流入, 从 Status中流入, 在Settings中流出
-  StreamController<BluetoothDevice> _transportDevice = StreamController.broadcast();
+  StreamController<BluetoothDevice> _transportDevice =
+      StreamController.broadcast();
+
   StreamSink<BluetoothDevice> get inAddConnectedDevice => _transportDevice.sink;
+
   Stream<BluetoothDevice> get outConnectedDevice => _transportDevice.stream;
 
-//  // 设备升级 流
-//  StreamController _updateFirmware = StreamController();
-//  StreamSink get inAdd
+//  // 设备升级 流, 只是用来展示 固件升级的进度
+  StreamController<UpdateProgressInfo> _updateFirmware = StreamController();
 
-  SettingsPageBloc(){
+  StreamSink<UpdateProgressInfo> get inAddUpdateProgress =>
+      _updateFirmware.sink;
+
+  Stream<UpdateProgressInfo> get outUpdateProgress => _updateFirmware.stream;
+
+  SettingsPageBloc() {
 //    outUpdateProgress.listen(onData)
   }
+}
+
+class UpdateProgressInfo {
+  final UpdatePhase updatePhase;
+  final double totalProgress;
+
+  UpdateProgressInfo(this.updatePhase, this.totalProgress);
+}
+
+enum UpdatePhase {
+  DOWNLOAD_FIRM,  //2%
+  FIND_SERVICE, // 2%
+  OPEN_CHARA, // 2%
+  SEND_HEAD, // 2%
+  SEND_FIRM, // 90%
+  RECEIVE_RESULT, // 2%
 }
--- lib/page/settings_page.dart
+++ lib/page/settings_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:race_demo/bloc/base_bloc.dart';
@@ -9,6 +11,7 @@ import 'package:race_demo/widget/text_divider_widget.dart';
 class SettingsPage extends StatelessWidget {
   final String title;
   final HomeBloc homeBloc;
+
   const SettingsPage(this.homeBloc, {Key key, this.title}) : super(key: key);
 
   @override
@@ -61,36 +64,44 @@ class SettingsPage extends StatelessWidget {
     );
   }
 
-  _buildUpgradeFirmware(BuildContext context, HomeBloc bloc, SettingsPageBloc settingsBloc) {
-
+  _buildUpgradeFirmware(
+      BuildContext context, HomeBloc homeBloc, SettingsPageBloc settingsBloc) {
     return RadiusContainer(
-      child: ListTile(
-        title: Text("Upgrade Firmware"),
-        trailing: StreamBuilder<BluetoothDevice>(
-          stream: bloc.outGetConnectedDevice,
-          initialData: null,
-          builder: (context, snap){
-            if(snap.data == null){
-              return RaisedButton(
-                child: Text("Please Connect Device"),
-                onPressed: null,// todo 点击之后就开始扫描并连接设备(去调用 Status那边的Bloc)
-              );
-            }
-            return RaisedButton(
-              child: Text("Check for updates"),
-              onPressed: ()=>_checkAndUpdateFirmware(snap.data, settingsBloc),
-            );
-          }
+        child: Column(
+      children: <Widget>[
+        ListTile(
+          title: Text("Upgrade Firmware"),
+          trailing: StreamBuilder<BluetoothDevice>(
+              stream: homeBloc.outGetConnectedDevice,
+              initialData: null,
+              builder: (context, snap) {
+                if (snap.data == null) {
+                  return RaisedButton(
+                    child: Text("Please Connect Device"),
+                    onPressed: null, // todo 点击之后就开始扫描并连接设备(去调用 Status那边的Bloc)
+                  );
+                }
+                return RaisedButton(
+                  child: Text("Check for updates"),
+                  onPressed: () => _checkAndUpdateFirmware(
+                      snap.data, settingsBloc.inAddUpdateProgress),
+                );
+              }),
         ),
+        StreamBuilder<UpdateProgressInfo>(
+          stream: settingsBloc.outUpdateProgress,
+          initialData: UpdateProgressInfo(null, 0.0),
 
-      ),
-    );
+        )
+      ],
+    ));
   }
-  void _checkAndUpdateFirmware(BluetoothDevice device) {
-    print('SettingsPage._checkAndUpdateFirmware 升级按钮被点击了! 当前已连接的设备: ${device.name} ');
+
+  void _checkAndUpdateFirmware(BluetoothDevice device,
+      StreamSink<UpdateProgressInfo> inAddUpdateProgress) {
+    print(
+        'SettingsPage._checkAndUpdateFirmware 升级按钮被点击了! 当前已连接的设备: ${device.name} ');
     // TODO 检查固件版本
     // 升级固件
-
-
   }
 }
