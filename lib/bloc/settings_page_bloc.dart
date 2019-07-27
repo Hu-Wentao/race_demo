0fd8fbc6a8b2ff9739dfb55b5d659ed5af7de43a
huwentao <hu.wentao@outlook.com>
2019-07-27 12:23:46 +0800

    更新固件下载链接

--- lib/bloc/settings_page_bloc.dart
+++ lib/bloc/settings_page_bloc.dart
@@ -188,10 +188,11 @@ enum UpdatePhase {
 }
 
 Future<File> _getFirmwareFromNet() async {
-//  const String downloadUrl = "https://send.firefox.com/api/download/87f7676cd7dbf541";  //OAD1_16 https://send.firefox.com/download/87f7676cd7dbf541/#tN-BTbkitGjT3kLZG-Qxkg
-//  const String downloadUrl = "https://send.firefox.com/api/download/ec51026b789212a0";  //OAD1_32
-  const String downloadUrl = "https://send.firefox.com/api/download/87f7676cd7dbf541";  //OAD2_16
-//  const String downloadUrl = "https://send.firefox.com/api/download/7780eb37e7a236a5";  //OAD2_32
+//  const String downloadUrl ="https://raw.githubusercontent.com/Hu-Wentao/File_Center/master/app_OAD1_16.bin";
+  const String downloadUrl ="https://raw.githubusercontent.com/Hu-Wentao/File_Center/master/app_OAD2_16.bin";
+
+//  const String downloadUrl ="https://raw.githubusercontent.com/Hu-Wentao/File_Center/master/app_OAD1_32.bin";
+//  const String downloadUrl ="https://raw.githubusercontent.com/Hu-Wentao/File_Center/master/app_OAD2_32.bin";
   Directory dir = await getApplicationDocumentsDirectory();
 
   File f = new File(dir.path + "/firmware.bin");
